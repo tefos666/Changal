c53b1a40302d060bc7cc7745a480ed8b9a6b4b12
Maghsoudi <mohsen.maghsoudi@gmail.com>
2024-01-13 04:19:43 -0800

    Add project files.

---
+++ Endpoint.Win/FrmMain.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Endpoint.Win
+{
+    public partial class FrmMain : FrmPattern
+
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+            this.FormTitle = "چنگال [فرم اصلی]";
+            this.WindowState = FormWindowState.Maximized;
+        }
+
+        private void FrmMain_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblSignOut_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
